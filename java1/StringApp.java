677bf84377141c25d0b1e0af82452b52f4d72266
junhwanyun <junhwanyun9356@gmail.com>
2021-01-11 19:47:25 +0900

    feat: 6-4. 문자열의 표현

---
+++ java1/StringApp.java
@@ -0,0 +1,17 @@
+public class StringApp {
+    public static void main(String[] args) {
+        // Chracter vs String
+        System.out.println("Hello World"); // String
+        System.out.println('H'); // Character
+        System.out.println("H");
+
+        System.out.println("Hello "
+            + "World");
+        
+        // new line
+        System.out.println("Hello \nWorld");
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
