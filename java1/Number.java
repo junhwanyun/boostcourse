460c44b4a7a5629e8c1aeae214a10bd990c2f20f
junhwanyun <junhwanyun9356@gmail.com>
2021-01-11 19:46:50 +0900

    feat: 6-3. 숫자와 연산

---
+++ java1/Number.java
@@ -0,0 +1,13 @@
+public class Number {
+    public static void main(String[] args) {
+        System.out.println(4 + 2); // 6
+        System.out.println(4 - 2); // 2
+        System.out.println(4 * 2); // 8
+        System.out.println(4 / 2); // 2
+
+        System.out.println(Math.PI); // 3.141592653589793
+
+        System.out.println(Math.floor(Math.PI));
+        System.out.println(Math.ceil(Math.PI));
+    }
+}
