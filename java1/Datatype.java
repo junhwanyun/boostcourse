105977c7ecdb20552084aaa48ba22a6815846590
junhwanyun <junhwanyun9356@gmail.com>
2021-01-11 19:46:08 +0900

    feat: 6-2. 데이터 타입

---
+++ java1/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println(7); // Number
+        System.out.println("seven"); // String
+        System.out.println("7"); // String 7
+
+        System.out.println(7 + 7); // 14
+        System.out.println("7" + "7"); // 77
+
+        System.out.println(7 * 7); // 49
+        // System.out.println("7" * "7");
+
+        System.out.println("7777".length()); // 4
+        // System.out.println(7777.length());
+    }
+}
