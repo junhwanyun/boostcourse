b9be9c617e2dcfe9a7e4e7b6570ea53e16868ec5
junhwanyun <junhwanyun9356@gmail.com>
2021-01-13 20:29:50 +0900

    feat: 8-2. 변수의 효용

---
+++ java1/Letter.java
@@ -0,0 +1,9 @@
+public class Letter {
+    public static void main(String[] args) {
+        String name = "junhwan";
+        System.out.println("Hello! " + name + ". ... " + name + " ... Best regards.");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
