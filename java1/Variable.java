dab53eeef3d6b40aceffd605e7801cc8f35dff4e
junhwanyun <junhwanyun9356@gmail.com>
2021-01-13 20:21:37 +0900

    feat: 8-1. 변수의 정의

---
+++ java1/Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // Integer
+        System.out.println(a);
+
+        double b = 1.1; // Real number
+        System.out.println(b);
+
+        String c = "Hello World!";
+        System.out.println(c);
+    }
+}
