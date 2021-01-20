f6d544e5b7bde650a07302487b9f26efc9dcb50e
junhwanyun <junhwanyun9356@gmail.com>
2021-01-20 19:19:56 +0900

    feat: 13-3. 자바 문서 보는 법 - 클래스

---
+++ java1/ClassApp.java
@@ -0,0 +1,7 @@
+public class ClassApp {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(7.7));
+        System.out.println(Math.ceil(7.7));
+    }
+}
