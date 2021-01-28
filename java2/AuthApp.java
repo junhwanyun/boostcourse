5555d109414c060a349d1322fe40822f4e4a8947
junhwanyun <junhwanyun9356@gmail.com>
2021-01-28 20:53:07 +0900

    fix: 4.3. 조건문 응용 2

--- java2/AuthApp.java
+++ java2/AuthApp.java
@@ -3,10 +3,23 @@ public class AuthApp {
         String id = "junhwanyun";
         String inputId = args[0];
 
+        String password = "777";
+        String inputPassword = args[1];
+
         System.out.println("Hello,");
 
         // if(inputId == id) {
-        if(inputId.equals(id)) {
+        // if(inputId.equals(id)) {
+        //     if(inputPassword.equals(password)) {
+        //         System.out.println("Master!");
+        //     }else {
+        //         System.out.println("Wrong password!");
+        //     }
+        // }else {
+        //     System.out.println("Who are you?");
+        // }
+
+        if(inputId.equals(id) && inputPassword.equals(password)) {
             System.out.println("Master!");
         }else {
             System.out.println("Who are you?");
