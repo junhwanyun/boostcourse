2e00ee24e51e030bee73ba3ddacbdf909e0f7949
junhwanyun <junhwanyun9356@gmail.com>
2021-02-03 18:27:35 +0900

    fix: 8-2. 종합응용 2

--- java2/AuthApp3.java
+++ java2/AuthApp3.java
@@ -1,13 +1,19 @@
 public class AuthApp3 {
     public static void main(String[] args) {
-        String[] users = {"junhwanyun", "junhwan", "yun"};
+        //String[] users = {"junhwanyun", "junhwan", "yun"};
+        String[][] users = {
+            {"junhwanyun", "111"},
+            {"junhwan", "222"},
+            {"yun", "333"}
+        };
 
         String inputId = args[0];
+        String inputPassword = args[1];
 
         boolean isLogined = false;
 
         for(int i = 0; i < users.length; i++) {
-            if(users[i].equals(inputId)) {
+            if(users[i][0].equals(inputId) && users[i][1].equals(inputPassword)) {
                 isLogined = true;
 
                 break;
@@ -15,9 +21,9 @@ public class AuthApp3 {
         }
 
         System.out.println("Hello,");
-        
+
         if(isLogined) {
-            System.out.println("yunhwanyun!");
+            System.out.println("junhwanyun!");
         }else {
             System.out.println("Who are you?");
         }
