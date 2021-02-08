be8247d4b764418907d922e7535c5e8680276048
junhwanyun <junhwanyun9356@gmail.com>
2021-02-08 18:05:40 +0900

    feat: 3. 메소드의 기본 형식

---
+++ java2/WhyMethodApp.java
@@ -0,0 +1,16 @@
+public class WhyMethodApp {
+    public static void main(String[] args) {
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+        // 100000000
+        printTwoTimesA();
+    }
+
+    public static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("A");
+        System.out.println("A");
+    }
+}
